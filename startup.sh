756fc7d4c8be86318350d5268b7d9d2715b3d46a
Giovanni B. Previatti <gpreviatt@gmail.com>
2020-08-09 09:44:29 -0300

    update and remove packages
    * del check if you want to configure vim in git
    * del translator-shell (developers don't needs this)
    * del wget (in doc you need to use wget to download the script, doesen't make sense you download this)

--- startup.sh
+++ startup.sh
@@ -1,44 +1,38 @@
 sudo apt-get update
 
-echo 'installing curl wget and snapd' 
-sudo apt install curl wget snapd-y
+echo 'installing curl and snapd' 
+sudo apt install curl -y
+
+echo 'installing snap package manager'
+sudo apt install snapd -y 
 
 echo 'installing git' 
 sudo apt install git -y
+
+echo 'installing terminator'
+sudo apt-get install terminator -y
+
+echo 'installing vim'
+sudo apt install vim -y
 
 echo "What name do you want to use in GIT user.name?"
 echo "For example, mine will be \"Giovanni Brunno Previatti\""
 read git_config_user_name
 git config --global user.name "$git_config_user_name"
-clear 
 
 echo "What email do you want to use in GIT user.email?"
 echo "For example, mine will be \"gpreviatt@gmail.com\""
 read git_config_user_email
 git config --global user.email $git_config_user_email
-clear
-
-echo "Can I set VIM as your default GIT editor for you? (y/n)"
 read git_core_editor_to_vim
-if echo "$git_core_editor_to_vim" | grep -iq "^y" ;then
-	git config --global core.editor vim
-else
-	echo "Okay, no problem. :) Let's move on!"
-fi
 
 echo 'installing zsh'
 sudo apt-get install zsh -y
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 chsh -s /bin/zsh
 
-echo 'installing vim'
-sudo apt install vim -y
-
 echo 'installing vscode by snap package'
 sudo snap install code --classic
-
-echo 'installing vscode extensions'
-code --install-extension shan.code-settings-sync
 
 echo 'installing postman by snap package'
 sudo snap install postman
@@ -50,49 +44,27 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 echo 'installing PHP'
 sudo apt-get install php php-common php-cli php-fpm php-xml php-zip php-mbstring php-gd php-curl php-codesniffer
 
-echo 'installing Composer'
-if hash composer; then
-  echo "Composer Already Installed"
-  echo "You can find it at $(type -p composer)"
-  exit
-fi
-
 # Download latest composer snapshot and run it by php
 sudo wget https://getcomposer.org/composer.phar
 sudo mv composer.phar /bin/composer
 echo "Composer Installed Globally Successfully"
-
-echo 'installing nvm' 
-sh -c "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash)"
-
-export NVM_DIR="$HOME/.nvm" && (
-git clone https://github.com/creationix/nvm.git "$NVM_DIR"
-cd "$NVM_DIR"
-git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-) && \. "$NVM_DIR/nvm.sh"
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-source ~/.zshrc
-nvm install --lts
-node --version
-npm --version
 
 echo 'installing zsh autosuggestions' 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 source ~/.zshrc
 
-echo 'installing terminator'
-sudo apt-get install terminator -y
-
-echo 'installing translate-shell'
-sudo apt install translate-shell
+echo 'installing nvm' 
+git clone https://github.com/lukechilds/zsh-nvm.git ~/.zsh-nvm
+source ~/.zsh-nvm/zsh-nvm.plugin.zsh
+nvm install --lts
+node --version
+npm --version
 
 echo 'installing docker'
 sudo apt install docker docker.io docker-compose -y
-sudo sudo chmod -aG docker $USER
+sudo groupadd docker
+sudo usermod -aG docker $USER
 echo 'you can use docker after rebot machine'
 
 echo 'installing dbeaver'
